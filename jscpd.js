e977428fc6956e9638ba3a0deb2884c908b5f488
Iusiel <iusiel@outlook.com>
2022-11-23 08:48:44 +0800

    Updated output directory of jscpd report

--- jscpd.js
+++ jscpd.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const { exec } = require("child_process");
 
 exec(
-  'npx jscpd --reporters "json" --output reports/ resources/js',
+  'npx jscpd --reporters "json" --output linters/reports/ resources/js',
   function (err, stdout, stderr) {
     if (stdout.indexOf("Clone found") !== -1) {
       process.exit(1);
